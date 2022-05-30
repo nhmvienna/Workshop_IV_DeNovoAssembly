9e12f0b028ace360dcbac51e4b0badf647724971
capoony <capoony@gmail.com>
2022-05-30 08:39:09 +0200

    init

---
+++ shell/assembly_pipeline.sh
@@ -0,0 +1,3 @@
+#### Assembly pipeline ####
+
+### In this workshop, we will to reconstruct continuous (i.e. syntenic) stretches of DNA (so called contigs) from the genome of the fish Garra longipinnis (Luise's and Sandra's favourite pet's) using three types of sequencing data.
