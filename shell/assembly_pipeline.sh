4b73e8f581f397ebcfd0b0c40afd0169dba9e07d
capoony <capoony@gmail.com>
2022-06-01 08:46:18 +0200

    update

--- shell/assembly_pipeline.sh
+++ shell/assembly_pipeline.sh
@@ -4,6 +4,22 @@
 
 ### (1) Clone Github repository
 
-### As a first step, you will have cloned the GitHub repository of this workshop to your home directory. If this is not the case, you should do it now using the follwing command
+### As a first step, you will have cloned the GitHub repository of this workshop to your home directory. If this is not the case, you should do it now using the following command
 
 git clone https://github.com/nhmvienna/Workshop_IV_DeNovoAssembly
+
+### now, let's have a look at the data, what do we have?
+
+cd Workshop_IV_DeNovoAssembly/data/
+
+ls -l
+
+cd Illumina/
+
+ls -l
+
+### What do the raw Illumina raw data look like?
+
+gunzip -c Garra474_1.fq.gz | head -4
+
+## What do these top 4 rows
