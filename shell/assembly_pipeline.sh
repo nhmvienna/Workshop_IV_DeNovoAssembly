ca2476205f420698313c9318332157694e6cdb67
capoony <capoony@gmail.com>
2022-06-01 09:55:18 +0200

    update

--- shell/assembly_pipeline.sh
+++ shell/assembly_pipeline.sh
@@ -1,6 +1,6 @@
 ############################ Assembly pipeline ####################################
 
-### In this workshop, we will to reconstruct continuous (i.e. syntenic) stretches of DNA (so called contigs) from the genome of the fish Garra longipinnis (Luise's and Sandra's favourite pet's) using three types of sequencing data.
+### In this workshop, we will reconstruct continuous (i.e. syntenic) stretches of DNA (so called contigs) from the genome of the fish Garra longipinnis (Luise's and Sandra's favourite pet's) using three types of sequencing data.
 
 ################### (1) Clone Github repository ###################
 
@@ -10,7 +10,7 @@ git clone https://github.com/nhmvienna/Workshop_IV_DeNovoAssembly
 
 ### now, let's have a look at the data, what do we have?
 
-cd Workshop_IV_DeNovoAssembly/data/
+cd Workshop_IV_DeNovoAssembly/data
 
 ls -l
 
@@ -28,4 +28,85 @@ gunzip -c Garra474_1.fq.gz | head -4
 
 ################### (2) DATA Quality ###################
 
-###
+### Next, we will examine the data quality of the Illumina dataset using the program FASTQC
+
+mkdir -p ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC
+
+echo """
+#!/bin/sh
+
+## name of Job
+#PBS -N fastqc
+
+## Redirect output stream to this file.
+#PBS -o ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC/fastq_log.txt
+
+## Stream Standard Output AND Standard Error to outputfile (see above)
+#PBS -j oe
+
+## Select 10 cores and 50gb of RAM
+#PBS -l select=1:ncpus=10:mem=50g
+
+######## load dependencies #######
+
+module load Tools/FastQC-0.11.9
+
+## loop through all raw FASTQ and test quality
+
+fastqc \
+  --outdir ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC \
+  --threads 10 \
+  ~/Workshop_IV_DeNovoAssembly/data/Illumina/Garra474_1.fq.gz \
+  ~/Workshop_IV_DeNovoAssembly/data/Illumina/Garra474_2.fq.gz
+
+""" > ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC/fastqc.sh
+
+## Submit the job to OpenPBS
+
+qsub ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC/fastqc.sh
+
+## check the status of your OpenPBS Job
+
+qstat -awt
+
+## once the job is finished, you can check the output in the browser
+
+firefox ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC/Garra474_1_fastqc.html
+
+## What about the ONT dataset? We will use Nanoplot for this!
+
+
+mkdir -p ~/Workshop_IV_DeNovoAssembly/results/ONT_QC
+
+echo """
+#!/bin/sh
+
+## name of Job
+#PBS -N fastqc
+
+## Redirect output stream to this file.
+#PBS -o ~/Workshop_IV_DeNovoAssembly/results/ONT_QC/fastq_log.txt
+
+## Stream Standard Output AND Standard Error to outputfile (see above)
+#PBS -j oe
+
+## Select 10 cores and 50gb of RAM
+#PBS -l select=1:ncpus=10:mem=50g
+
+######## load dependencies #######
+
+module load Tools/FastQC-0.11.9
+
+## loop through all raw FASTQ and test quality
+
+fastqc \
+  --outdir ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC \
+  --threads 10 \
+  ~/Workshop_IV_DeNovoAssembly/data/Illumina/Garra474_1.fq.gz \
+  ~/Workshop_IV_DeNovoAssembly/data/Illumina/Garra474_2.fq.gz
+
+""" > ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC/fastqc.sh
+
+## Submit the job to OpenPBS
+
+qsub ~/Workshop_IV_DeNovoAssembly/results/Illumina_QC/fastqc.sh
