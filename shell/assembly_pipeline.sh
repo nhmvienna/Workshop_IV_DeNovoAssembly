24f72dbe338afd59e5a5d1c943785f527db0e38f
capoony <capoony@gmail.com>
2022-06-01 08:33:22 +0200

    update

--- shell/assembly_pipeline.sh
+++ shell/assembly_pipeline.sh
@@ -1,3 +1,9 @@
 #### Assembly pipeline ####
 
 ### In this workshop, we will to reconstruct continuous (i.e. syntenic) stretches of DNA (so called contigs) from the genome of the fish Garra longipinnis (Luise's and Sandra's favourite pet's) using three types of sequencing data.
+
+### (1) Clone Github repository
+
+### As a first step, you will have cloned the GitHub repository of this workshop to your home directory. If this is not the case, you should do it now using the follwing command
+
+git clone https://github.com/nhmvienna/Workshop_IV_DeNovoAssembly
